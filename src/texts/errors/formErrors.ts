674a7ee28d0a79ad2d9ae18ca17ca444937cf7aa
Richard Li <richardlidefa@gmail.com>
2022-01-05 15:32:49 +0800

    Create short common texts

---
+++ src/texts/errors/formErrors.ts
@@ -0,0 +1,6 @@
+// Missing input
+export const missingField = 'Please fill in this field!'
+
+// Invalid input
+export const invalidName = 'Please enter a valid name!' // numbers and symbols in names are invalid
+export const invalidEmail = 'Please enter a valid email address!'
