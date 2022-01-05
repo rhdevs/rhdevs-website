49c28587fdf10976c7d28de73c537e6e31f9bfde
Jason <jason.ng.zq99@gmail.com>
2022-01-06 00:06:18 +0800

    Add route variables

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,1 +1,11 @@
+import React from 'react'
+import { Route, Routes as Switch } from 'react-router-dom'
 
+const Home = React.lazy(() => import(/* webpackChunckName: "Home" */ '../pages/Home'))
+const About = React.lazy(() => import(/* webpackChunckName: "Home" */ '../pages/About'))
+const Articles = React.lazy(() => import(/* webpackChunckName: "Home" */ '../pages/Articles'))
+const Contact = React.lazy(() => import(/* webpackChunckName: "Home" */ '../pages/Contact'))
+const Events = React.lazy(() => import(/* webpackChunckName: "Home" */ '../pages/Events'))
+const Projects = React.lazy(() => import(/* webpackChunckName: "Home" */ '../pages/Projects'))
+
+export const Routes = () => <Switch></Switch>
