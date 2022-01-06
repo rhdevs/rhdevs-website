a7ac66b312f13bdcaf6778f5640754cc1aab23d6
Ang Ki Yong <angkiyong@gmail.com>
2022-01-06 22:11:17 +0800

    forgot to save the file before committing whoops

---
+++ src/texts/descriptions/aboutUs.js
@@ -0,0 +1,5 @@
+export const missionDescription = 'A closely-knitted community of members with different niches and readily-available mentors of different stages'
+
+export const visionDescription = 'To accelerate tech-innovation, learning and adoption in Raffles Hall'
+
+export const logoDescription = '</> symbols are core in the HTML language for webpage, forming the D in DEVS\n\nGradient element: Includes the CCA\'s design aspect and mirrors "futuristic" color trends in apps\' logos'
---
+++ src/texts/descriptions/projects.js
@@ -0,0 +1,5 @@
+export const rhApp = 'An application aimed at bettering the lives of Raffles Hall Residents. Currently working on the design of the interface of the application.'
+
+export const hackathons = 'Annual Inter-Hall Hackathons are held to foster healthy competitions and relationships amongst developers of different halls.'
+
+export const rhAnnouncementBot = 'We created a telegram bot to assist Raffles Hall CCA leaders with sending out hall wide announcements.'
