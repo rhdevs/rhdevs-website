d24dbf3ef490e997842c0dcd55329080d3f85da8
Wang Yongbin <yongbin_01@hotmail.com>
2022-01-07 20:57:01 +0800

    Added over styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Button as AntdButton } from 'antd'
 import { BaseButton } from './styles/Button.styled'
 
-type Props = {
-  text: string
-  link: string
-}
+const StyledButton = styled(AntdButton)`
+  height: 40px;
+  font-size: 26px;
+  border-width: 1px;
+  background-color: black;
+  color: white;
+  padding: 2px;
+  &:hover {
+    border-bottom: 1px solid red;
+  }
+`
 
-function Button(props: Props) {
+function Button() {
   return (
+    // <StyledButton>Facebook</StyledButton>
     <BaseButton>
-      <AntdButton type="text" onClick={() => () => window.open(`${props.link}`, '_blank')}>
-        {props.text}
-      </AntdButton>
+      <AntdButton type="text">Facebook</AntdButton>
     </BaseButton>
   )
 }
