904d01a7bffc3692fb43e2aeb7653445728070ef
Zhang Zhikai <zhangzhikai71@gmail.com>
2022-01-07 17:33:40 +0800

    Add props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,25 +1,18 @@
 import React from 'react'
-import styled from 'styled-components'
 import { Button as AntdButton } from 'antd'
 import { BaseButton } from './styles/Button.styled'
 
-const StyledButton = styled(AntdButton)`
-  height: 40px;
-  font-size: 26px;
-  border-width: 1px;
-  background-color: black;
-  color: white;
-  padding: 2px;
-  &:hover {
-    border-bottom: 1px solid red;
-  }
-`
+type Props = {
+  text: string
+  link: string
+}
 
-function Button() {
+function Button(props: Props) {
   return (
-    // <StyledButton>Facebook</StyledButton>
     <BaseButton>
-      <AntdButton type="text">Facebook</AntdButton>
+      <AntdButton type="text" onClick={() => () => window.open(`${props.link}`, '_blank')}>
+        {props.text}
+      </AntdButton>
     </BaseButton>
   )
 }
