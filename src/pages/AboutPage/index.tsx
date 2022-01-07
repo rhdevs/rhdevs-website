0eec1e03cc7524034a42fa279d6badd6387e24f4
Maxim Yam <maximyam@gmail.com>
2022-01-07 12:19:44 +0800

    About Page template

---
+++ src/pages/AboutPage/index.tsx
@@ -0,0 +1,12 @@
+import { MainContainer } from './styles/AboutPage.styled'
+import { PageSectionComponent } from '../../components/PageSection'
+
+export default function AboutPage() {
+  return (
+    <MainContainer>
+      <PageSectionComponent></PageSectionComponent>
+      <PageSectionComponent></PageSectionComponent>
+      <PageSectionComponent></PageSectionComponent>
+    </MainContainer>
+  )
+}
---
+++ src/pages/AboutPage/styles/AboutPage.styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  height: 100%;
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+`
