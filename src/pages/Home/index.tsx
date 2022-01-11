52f1e9a9107ebfa8bc0bb11c5bc470c4be848ba7
Magmanat <princemjp@gmail.com>
2022-01-11 23:40:16 +0800

    added a defaultpath variable example

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import raw from 'raw.macro'
 
 export default function Home() {
-  const textfile = raw('../../texts/text.txt')
-  return <div>{textfile || 'Loading...'}</div>
+  const defaultpath = '../../texts/'
+  const textfile = raw(`${defaultpath}text.txt`)
+  return <div>{textfile}</div>
 }
