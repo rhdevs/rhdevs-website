273a8a57d4ad78ef92e079c5217b2c8cbf7ffd1b
HP <marcus.ong.qy@gmail.com>
2022-01-16 09:01:58 +0800

    remove test code on home index

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,3 @@
-import { useState } from 'react'
-import ClickToCopySection from '../../components/ClickToCopySection/ClickToCopySection'
-import InputField from '../../components/InputField/InputField'
-
 export default function Home() {
-  const [value, setValue] = useState('default')
-  return (
-    <div style={{ background: 'black', height: '100vh' }}>
-      <InputField title="Title" value={value} setValue={setValue} />
-      <ClickToCopySection text="Text" title="Title" />
-    </div>
-  )
+  return <>Home Page</>
 }
