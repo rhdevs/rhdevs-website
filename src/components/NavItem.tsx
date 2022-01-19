c206f04c124454b8089a60c7986d5da1d52e13e7
chun <lamchunyu00@gmail.com>
2022-01-19 10:35:33 +0800

    Updated NavItem to use theme typography

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useTheme } from 'styled-components'
 import { NavDiv } from './styles/NavItem.styled'
 
 type NavItemProps = {
@@ -13,8 +13,10 @@ const defaultProps = {
 }
 
 export default function NavItem({ text, isActive, onClick }: NavItemProps) {
+  const theme = useTheme()
+  const { h3 } = { ...theme.typography.fontSize }
   return (
-    <NavDiv isActive={isActive} onClick={onClick}>
+    <NavDiv isActive={isActive} onClick={onClick} fontType={h3}>
       {text}
     </NavDiv>
   )
