331775236ebf47b91560ef1fae5788bc73a7f9b9
tinenhao <tinenhao@u.nus.edu>
2022-01-22 00:39:54 +0800

    reverted About Page to normal

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,6 +1,3 @@
-import PageSectionComponent from '../../components/PageSection'
-import image from '../../assets/noimg.png'
-
 export default function About() {
-  return <PageSectionComponent title="test" description="sdjhgsajkhdfjhsdhfjshjfhd" position="right" image={image} />
+  return <>About Page</>
 }
