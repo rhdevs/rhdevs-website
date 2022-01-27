ee452ec021db9cd49dd9f845170c16390dda9d9a
Tai <kktai1512@outlook.com>
2022-01-27 23:31:05 +0800

    add fade in

--- src/pages/Events/styles/EventPage.styled.tsx
+++ src/pages/Events/styles/EventPage.styled.tsx
@@ -7,4 +7,25 @@ export const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: space-between;
+  > * {
+    @keyframes fadeIn {
+      from {
+        opacity: 0;
+      }
+
+      to {
+        opacity: 1;
+      }
+    }
+
+    -webkit-animation-duration: 1s;
+    animation-duration: 1s;
+    -webkit-animation-fill-mode: both;
+    animation-fill-mode: both;
+
+    -webkit-animation-delay: 0.5s;
+    -moz-animation-delay: 0.5s;
+    animation-delay: 0.5s;
+    animation-name: fadeIn;
+  }
 `
