340f8650e1c925102f0411771da38bc5165e4101
Marcus Ong <marcus.ong.qy@gmail.com>
2022-01-30 22:24:47 +0800

    added fade animation for tooltip

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -20,9 +20,6 @@ import {
   TooltipStyled,
 } from './styles/InputField.styled'
 
-const VISIBLE = '1'
-const INVISIBLE = '0'
-
 type Props = {
   title: string
   value: string
@@ -50,6 +47,11 @@ const customBrowserMessages: Record<string, string> = {
   name: nameBrowserMessage,
 }
 
+// tooltip fade-in/display/fade-out times, in seconds
+const FADE_IN_TIME = 0.3
+const DISPLAY_TIME = 4
+const FADE_OUT_TIME = 0.7
+
 function InputField(props: Props) {
   const theme = useTheme()
   const { title, value, setValue, required } = props
@@ -73,19 +75,35 @@ function InputField(props: Props) {
     setLabelElement(required ? document.getElementById(labelId) : null)
   }, [labelId])
 
+  /* eslint-disable no-param-reassign */
+  const fadeIn = (labelElem: HTMLElement) => {
+    // tooltip appear animation
+    labelElem.style.visibility = 'visible'
+    labelElem.style.opacity = '1'
+    labelElem.style.transition = `opacity ${FADE_IN_TIME}s linear`
+  }
+
+  /* eslint-disable no-param-reassign */
+  const fadeOut = (labelElem: HTMLElement) => {
+    // tooltip disappear animation
+    labelElem.style.visibility = 'hidden'
+    labelElem.style.opacity = '0'
+    labelElem.style.transition = `visibility 0s ${FADE_OUT_TIME}s, opacity ${FADE_OUT_TIME}s linear`
+  }
+
   const onInput = (e: any, callback = false) => {
     // supposed to be (e: React.FormEvent<HTMLInputElement>) but TS will highlight (e.target.setCustomValidity) as invalid, that's why (any)
     if (required && labelElement) {
       if (!callback) e.target.setCustomValidity('')
-      labelElement.style.opacity = INVISIBLE // hide tooltip
+      fadeOut(labelElement)
     }
   }
 
   const onInvalid = async (e: any) => {
     if (required && labelElement) {
-      labelElement.style.opacity = VISIBLE // show tooltip
+      fadeIn(labelElement)
       e.target.setCustomValidity(customInvalidMsg) // overrides custom message displayed by browser onInvalid
-      setTimeout(onInput, 2000, e, true) // hide tooltip after 2s
+      setTimeout(onInput, DISPLAY_TIME * 1000, e, true) // hide tooltip after 3s
     }
   }
 
