58908917c535a1cad07d262dcb899ce8477b575b
jason-ng-zq99 <jason.ng.zq99@gmail.com>
2022-02-20 19:44:05 +0800

    Add padding between social media row and sentence

--- src/pages/Contact/styles/Contact.styled.ts
+++ src/pages/Contact/styles/Contact.styled.ts
@@ -12,6 +12,7 @@ export const MainContainer = styled.div`
 export const StaticSection = styled.div`
   display: flex;
   justify-content: space-between;
+  padding-bottom: 110px;
 `
 
 export const TextSection = styled.div`
