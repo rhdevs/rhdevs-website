cedede4eb540ecb26f1af70089193c6fc5dc3795
Jason <jason.ng.zq99@gmail.com>
2022-03-04 23:26:44 +0800

    Edit missionDescription text

--- src/texts/descriptions/aboutUs.ts
+++ src/texts/descriptions/aboutUs.ts
@@ -1,5 +1,9 @@
 export const missionDescription =
-  'A closely-knitted community of members with different niches and readily-available mentors of different stages'
+  'Raffles Hall Developers aims to accelerate tech-innovation, learning, and adoption in Raffles Hall.' +
+  'With the development of products to improve residents’ hall life, we are introducing new processes to re-imagine our day-to-day activities.' +
+  'Continuously searching for areas of improvement and inconvenience, the applications and innovations we build are targeted at creating the best hall experience for RHesidents.' +
+  'We as a CCA pledge to continuously learn and grow through undergoing both training and application.' +
+  'Developers and Designers will strive to enhance their skills in front-end web development, back-end web development, UI/UX design, and graphic design.'
 
 export const visionDescription = 'To accelerate tech-innovation, learning and adoption in Raffles Hall'
 
