36f079e67bd9f864863ac97a555c455ceeb3f893
Jason <jason.ng.zq99@gmail.com>
2022-03-04 23:28:18 +0800

    Edit mission description text

--- src/texts/descriptions/aboutUs.ts
+++ src/texts/descriptions/aboutUs.ts
@@ -5,7 +5,10 @@ export const missionDescription =
   'We as a CCA pledge to continuously learn and grow through undergoing both training and application.' +
   'Developers and Designers will strive to enhance their skills in front-end web development, back-end web development, UI/UX design, and graphic design.'
 
-export const visionDescription = 'To accelerate tech-innovation, learning and adoption in Raffles Hall'
+export const visionDescription =
+  'Raffles Hall Developers was formed with the vision of becoming a closely-knitted community of members with different niches of interests and readily-accessible mentors of different stages.' +
+  'Whether you’re a Developer or Designer, RHDevs seamlessly works as a unit to explore and build ideas together.' +
+  'Seniors will be working hand with hand with junior members to learn from each other, and both offer and receive guidance as they face new and exciting challenges.'
 
 export const logoDescription =
   '</> symbols are core in the HTML language for webpage, forming the D in DEVS\n\nGradient element: Includes the CCA\'s design aspect and mirrors "futuristic" color trends in apps\' logos'
