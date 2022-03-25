777b88a807797fa147db34ae387d1596fa2082bd
Jason <jason.ng.zq99@gmail.com>
2022-03-25 11:42:46 +0800

    Change cursor attribute to default.
    Designer reflected that there is nothing to click for now

--- src/components/styles/ProjectPreview.styled.ts
+++ src/components/styles/ProjectPreview.styled.ts
@@ -40,6 +40,6 @@ export const PreviewWrapper = styled.div`
   display: flex;
   flex-direction: column;
   background-color: ${(props) => props.theme.palette.common.black};
-  cursor: pointer;
+  cursor: default;
   flex: 1 1 100%;
 `
